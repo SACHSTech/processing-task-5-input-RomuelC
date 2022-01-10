337c3115aa3fa1b9fb013ec8637b48d4989e11b3
RomuelC <romuel.cuevas23@ycdsbk12.ca>
2022-01-09 23:25:02 -0500

    Update Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,23 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  String message = "";
+
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(24, 101, 184);
+    textSize(50);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+ 
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    if (mousePressed) {
+      fill(156, 27, 191);
+      noStroke();
+      rect(mouseX - 40, mouseY - 40, 40, 40);
+    }
   }
-  
-  // define other methods down here.
 }
